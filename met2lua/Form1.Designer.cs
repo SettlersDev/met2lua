01669ffbfeb058dc9f48669a2c85b69d91fe8631
dbeinder <yoxxosh@gmail.com>
2016-05-01 14:19:19 +0200

    initial commit

---
+++ met2lua/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace met2lua
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbOut = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbOut
+            // 
+            this.tbOut.AllowDrop = true;
+            this.tbOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbOut.BackColor = System.Drawing.SystemColors.Window;
+            this.tbOut.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbOut.Location = new System.Drawing.Point(12, 12);
+            this.tbOut.Multiline = true;
+            this.tbOut.Name = "tbOut";
+            this.tbOut.ReadOnly = true;
+            this.tbOut.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.tbOut.Size = new System.Drawing.Size(516, 301);
+            this.tbOut.TabIndex = 0;
+            this.tbOut.Text = "Drop a .met file here!";
+            this.tbOut.DragDrop += new System.Windows.Forms.DragEventHandler(this.tbOut_DragDrop);
+            this.tbOut.DragEnter += new System.Windows.Forms.DragEventHandler(this.tbOut_DragEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 325);
+            this.Controls.Add(this.tbOut);
+            this.Name = "Form1";
+            this.Text = "Metrics to Lua Converter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbOut;
+    }
+}
+
---
+++ met2lua/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace met2lua
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbOut_DragDrop(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.None;
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length != 1 || Path.GetExtension(files[0]) != ".met")
+            {
+                MessageBox.Show("Please drop ONE .met file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            string[] lines = File.ReadAllLines(files[0]);
+            Size[] chars = new Size[256];
+            int validHeight = -1;
+
+            for (int ln = 4; ln < lines.Length; ln++)
+            {
+                string[] pieces = lines[ln].Split(new char[] { '\t', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (pieces.Length < 5)
+                    continue;
+                int ascii = int.Parse(pieces[0]);
+                if (ascii >= 256)
+                    break;
+
+                Size chr = new Size(
+                    1 + int.Parse(pieces[3]) - int.Parse(pieces[1]),
+                    1 + int.Parse(pieces[4]) - int.Parse(pieces[2]));
+                chars[ascii] = chr;
+
+                if (chr.Height > validHeight)
+                    validHeight = chr.Height;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(string.Format("font = {{\r\n\theight = {0},\r\n\twidths = [\r\n\t\t", validHeight)); 
+            for (int c = 0, ln = 0; c < 256; c++, ln++)
+            {
+                if (ln == 16)
+                {
+                    ln = 0;
+                    sb.Append("\r\n\t\t");
+                }
+
+                if (chars[c] != null)
+                {
+                    if (chars[c].Width < 10)
+                        sb.Append(' ');
+                    sb.Append(chars[c].Width);
+                }
+                else
+                    sb.Append(" 0");
+
+                if (c != 255)
+                    sb.Append(", ");
+            }
+
+            sb.Append("\r\n\t]\r\n}");
+            tbOut.Text = sb.ToString();
+        }
+
+        private void tbOut_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+    }
+
+    public class Size
+    {
+        public int Width;
+        public int Height;
+        public Size(int width, int height)
+        {
+            this.Height = height;
+            this.Width = width;
+        }
+    }
+}
